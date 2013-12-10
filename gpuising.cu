3825a8ef1359ac8e3bce4615165cf7c8383392c9
Daniel Kosalla <dejnon@gmail.com>
2013-12-10 21:37:04 +0100

    Adds working triangular distribution algorithm

--- gpuising.cu
+++ gpuising.cu
@@ -14,9 +14,9 @@
 
 #define LATICE_SIZE 60
 #define W0 0.5
-#define C 0.5
+#define C 0.1
 
-#define MAX_MTS 10
+#define MAX_MTS 100000
 
 #define TRUE 1
 #define FALSE 0
@@ -38,6 +38,36 @@
     }                                       \
     printf("]\n"); } while (0)
 
+#define BOND_DENSITY(LATICE) ({             \
+    int sum = 0;                            \
+    for (int i = 0; i < LATICE_SIZE; i++) { \
+        int next = (i+1) % LATICE_SIZE;     \
+        sum+=2*abs(LATICE[i]-LATICE[next]); \
+    }                                       \
+    ((float)sum / (float)(2*LATICE_SIZE));  \
+})
+
+#define IS_FERROMAGNETIC(LATICE) ({         \
+    short is_ferromagnetic = TRUE;          \
+    for (int i = 0; i < LATICE_SIZE; i++) { \
+        if (LATICE[0] != LATICE[i]) {       \
+            is_ferromagnetic = FALSE;       \
+            break;                          \
+        }                                   \
+    }                                       \
+    is_ferromagnetic;                       \
+})
+
+#define TRIANGLE_DISTRIBUTION(miu, sigma) ({   \
+        float start = max(miu-sigma, 0.0);     \
+        float end   = min(miu+sigma, 1.0);     \
+        double rand = (                        \
+            curand_uniform(&state[blockIdx.x]) \
+          + curand_uniform(&state[blockIdx.x]) \
+        ) / 2.0;                               \
+        ((end-start) * rand) + start;          \
+})
+
 #define SWAP(A, B, C) do {  \
     C = A;                  \
     A = B;                  \
@@ -52,9 +82,9 @@ __global__ void generate_kernel(curandStateMtgp32 *state)
     short * SWAP               = NULL;
 
     long  latice_update_counter = 0;
-    short is_latice_updated, is_ferromagnetic = FALSE;
-    int first_i, last_i         = 0;
-    long monte_carlo_steps      = 0;
+    short is_latice_updated     = FALSE;
+    int   first_i, last_i       = 0;
+    long  monte_carlo_steps     = 0;
 
     // Initialize LATICE as antiferromagnet [1,0,1,...]
     for (int i = 0; i < LATICE_SIZE; i++) {
@@ -62,55 +92,45 @@ __global__ void generate_kernel(curandStateMtgp32 *state)
     }
 
     while ( monte_carlo_steps < MAX_MTS ) {
-        if ( !is_latice_updated ) {
-            // If latice is in ferromagnetic state simulation can stop
-            is_ferromagnetic = TRUE;
-            for (int i = 0; i < LATICE_SIZE; i++) {
-                if (LATICE[0] != LATICE[i]) {
-                    is_ferromagnetic = FALSE;
-                }
-            }
-            if (is_ferromagnetic) {
-                break;
-            }
+        if ( !is_latice_updated && IS_FERROMAGNETIC(LATICE) ) {
+            // If latice is in ferromagnetic state, simulation can stop
+            break;
         }
-
-        // bondDensity(LATICE)
-        // miu, sigma via blockId or parameter?
-        // generator per block / per watek / host api
         first_i = (int)(LATICE_SIZE * curand_uniform(&state[blockIdx.x]));
         last_i  = (int)(first_i + (C * LATICE_SIZE));
         is_latice_updated = FALSE; // ?
-
         for (int i = 0; i < LATICE_SIZE; i++) {
-            if (first_i <= i && i <= last_i) {
-                
+            NEXT_STEP_LATICE[i] = LATICE[i];
+            if (first_i <= i && i <= last_i) {      
                 int left  = (i-1) % LATICE_SIZE;
                 int right = (i+1) % LATICE_SIZE;
 
-                if ( LATICE[left] == LATICE[right] && LATICE[i] != LATICE[left] ) {
-                    NEXT_STEP_LATICE[i] = LATICE[left];
-                    latice_update_counter++; is_latice_updated = TRUE;
-                } else if ( W0 > curand_uniform(&state[blockIdx.x]) ) {
+                // Neighbours are the same and different than the current spin
+                if ( LATICE[left] == LATICE[right] ) {
+                    NEXT_STEP_LATICE[i] = LATICE[left];                        
+                }
+                // Otherwise randomly flip the spin
+                else if ( W0 > TRIANGLE_DISTRIBUTION(0.5, 0.5) ) {
                     NEXT_STEP_LATICE[i] = FLIP_SPIN(LATICE[i]);
-                    latice_update_counter++; is_latice_updated = TRUE;
                 }
-            } else {
-                if (LATICE[i] != NEXT_STEP_LATICE[i]) {
-                    latice_update_counter++; is_latice_updated = TRUE;
-                }
-                NEXT_STEP_LATICE[i] = LATICE[i];
+            }
+            if (LATICE[i] != NEXT_STEP_LATICE[i]) {
+                latice_update_counter++; 
+                is_latice_updated = TRUE;
             }
         }
         monte_carlo_steps = (int)(latice_update_counter / LATICE_SIZE);
         SWAP(LATICE, NEXT_STEP_LATICE, SWAP);
     }
+
+    printf("%f\n", BOND_DENSITY(LATICE)); 
     PRINT_LATICE(LATICE);
     printf("%d\n", monte_carlo_steps);
 }
 
 int main(int argc, char *argv[])
 {
+
     curandStateMtgp32 *devMTGPStates;
     mtgp32_kernel_params *devKernelParams;
 
